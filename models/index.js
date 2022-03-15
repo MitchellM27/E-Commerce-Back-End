aef4613afcb4ad994c0e4ada6bff0c5c74713175
Mitchell <mitchellm27@gmail.com>
2022-03-15 16:12:44 -0700

    added models and routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: {
+    model: ProductTag,
+  },
+  onDelete: 'CASCADE',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKeyz: 'product_id',
+  through: {
+    model: ProductTag,
+  },
+  onDelete: 'CASCADE',
+})
 
 module.exports = {
   Product,
